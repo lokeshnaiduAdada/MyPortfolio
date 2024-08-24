7d75dcfdbf92ee36676d92856c3335432e1bbdba
lokesh naidu <lokeshnaiduadada@gmail.com>
2024-08-24 18:05:43 +0530

    new project added

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -54,6 +54,27 @@ const Project=()=>{
                                 </div>
                             </div>
                         </div>
+                        <div className='project3'>
+                            <div className='project3_image'>
+                                <img src='e-mart_view.png' alt='Project3_image' ></img>
+                            </div>
+                            <div className='project3_details'>
+                                <h3 style={{color:'red'}}>E-Mart Hub</h3>
+                                <p>E-Mart Hub, is an online shopping platform designed to provide a seamless and user-friendly experience for customers. The platform allows users to browse various product categories, add items to their cart, and complete purchases through a streamlined checkout process. Built using React, the front-end is responsive and optimized for performance, ensuring a smooth shopping experience across devices.</p>
+                                <div className='social_media3'>
+                                <motion.button whileHover={{scale:1.3}} className='button1'> 
+                                        <a href="https://github.com/lokeshnaiduAdada/E-Mart-Hub" target="_blank">
+                                            <svg viewBox="0 0 496 500" height="1.5rem" xmlns="http://www.w3.org/2000/svg"><path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg>   
+                                        </a> 
+                                    </motion.button>&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <motion.button whileHover={{scale:1.3}} >
+                                        <a href="https://e-mart-hub.netlify.app/" target="_blank">        
+                                            <svg viewBox="0 0 20 20" height="1.5rem" xmlns="http://www.w3.org/2000/svg"><path d="M11 3a1 1 0 100 2h2.586l-6.293 6.293a1 1 0 101.414 1.414L15 6.414V9a1 1 0 102 0V4a1 1 0 00-1-1h-5z"></path><path d="M5 5a2 2 0 00-2 2v8a2 2 0 002 2h8a2 2 0 002-2v-3a1 1 0 10-2 0v3H5V7h3a1 1 0 000-2H5z"></path></svg>
+                                        </a>
+                                    </motion.button>
+                                </div>
+                            </div>
+                        </div>
                     </motion.div>
                 </div>
             </section>
