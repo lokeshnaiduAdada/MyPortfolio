70a949d43a19acd0acfe67dab423a772029a6aa6
lokesh naidu <lokeshnaiduadada@gmail.com>
2025-01-09 13:04:51 +0530

    Experience Added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,6 @@
 import './Experience.css';
 import { motion } from 'framer-motion'; 
+import { AiFillCaretRight } from "react-icons/ai";
 const Experience=()=>{
     return(
         <section className="exp_grid" id="exp_container">
@@ -7,11 +8,27 @@ const Experience=()=>{
                 <motion.div 
                 initial={{opacity:0,x:-100}}
                 whileInView={{opacity:1,x:0}}
-                // animate={{opacity:1,x:0}}
                 transition={{duration:0.7,type:'spring',stiffness:220}}
                 className="exp_content">
                     <h2>EXPERIENCE</h2>
-                    <div className='exp_company'>
+                    <div className='exp_company2'>
+                        <div>
+                            <h2 style={{color:"red",marginBlockStart:"0.50em",marginBlockEnd:"0.50em"}}>Full Stack Internship</h2>
+                        </div>
+                        <div>
+                            <h3 style={{marginBlockStart:"0.50em",marginBlockEnd:"0.50em"}}>CodTech IT solutions, [June, 2024] - [sep, 2024]</h3>
+                        </div>
+                        <div>
+                            <ul style={{ color: 'gray', fontWeight: 500, listStyleType: 'disc', paddingLeft: '20px' }}>
+                                <li>Developed and maintained dynamic, high-performance web applications using Java, ReactJS, Spring Boot,Spring Cloud and Hibernate, enhancing user experience and application functionality.</li>
+                                <li>Gained hands-on experience in debugging, optimizing, and deploying code to production environments, ensuring reliable and efficient system performance.</li>
+                                <li>Acquired Thererical knowledge of microservices architecture, improving understanding of distributed systems and enabling scalable, modular application design.</li>
+                                <li>Collaborated with a team of developers to deliver scalable back-end solutions, boosting system performance by 25% through optimization and architectural improvements.</li>
+                                <li>Earned certifications in Java Full Stack Development, demonstrating a strong commitment to continuous learning and staying updated with emerging technologies.</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className='exp_company1'>
                         <div>
                             <h2 style={{color:"red",marginBlockStart:"0.50em",marginBlockEnd:"0.50em"}}>Full Stack Intern</h2>
                         </div>
@@ -19,19 +36,13 @@ const Experience=()=>{
                             <h3 style={{marginBlockStart:"0.50em",marginBlockEnd:"0.50em"}}>Sathya Technology, [June, 2023] - [Dec, 2023]</h3>
                         </div>
                         <div>
-                            <p style={{color: 'gray',fontWeight:500}}>
-                                During my six-month internship at Sathya Technology in Java 
-                                Full Stack development, I acquired comprehensive knowledge and 
-                                hands-on experience in various technologies and frameworks, 
-                                including Java, Spring Framework, Hibernate, JavaScript, HTML/CSS, 
-                                and more. I actively participated in project work, collaborating 
-                                with my peers to develop web applications that showcased my 
-                                skills in both front-end and back-end development. Additionally, 
-                                I received recognition for my dedication and commitment to learning, 
-                                earning certificates in various aspects of Java Full Stack development. 
-                                This internship experience has greatly enhanced my capabilities 
-                                and prepared me for a career in software development.
-                            </p>
+                        <ul style={{ color: 'gray', fontWeight: 500, listStyleType: 'disc', paddingLeft: '20px' }}>
+                                <li>Gained hands-on experience in Java Full Stack development, working with technologies such as Java, Spring Framework, Hibernate, JavaScript, HTML/CSS, and SQL.</li>
+                                <li>Actively contributed to project development, collaborating with peers to build dynamic web applications, integrating both front-end and back-end components.</li>
+                                <li>Focused on enhancing user experience through JavaScript, HTML/CSS, and responsive design, while building robust server-side logic using Spring Boot and Hibernate.</li>
+                                <li>Developed expertise in database management and RESTful API integration, improving application functionality and performance.</li>
+                                <li>Recognized for dedication to learning and growth, earning certifications in Java Full Stack Development, which showcased my ability to master diverse technologies.</li>
+                            </ul>
                         </div>
                     </div>
                 </motion.div>
